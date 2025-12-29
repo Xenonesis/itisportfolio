d191952d75bd738b5780a134b1f084b1aefc8d59
Xenonesis <ffjawed@gmail.com>
2025-12-29 08:22:54 +0530

    Update portfolio with Aditya Kumar Tiwari's information

---
+++ src/utilities/socialMedia.ts
@@ -0,0 +1,20 @@
+const socialMediaLinks = [
+  {
+    name: "github",
+    link: "https://github.com/Xenonesis",
+  },
+  {
+    name: "x",
+    link: "https://x.com/itisaddy7",
+  },
+  {
+    name: "email",
+    link: "mailto:itisaddy7@gmail.com",
+  },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/itisaddy/",
+  },
+];
+
+export default socialMediaLinks;
